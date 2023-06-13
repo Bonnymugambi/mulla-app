c00beecfcb06efef7eddd40ba8d4df42448015af
Boniface Mugambi <104928927+Bonnymugambi@users.noreply.github.com>
2023-06-13 10:48:11 +0300

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  menu_icon_variable:boolean=false;
+
+  menuVariable:boolean=false;
+  openMenu(){ 
+    this.menuVariable != this.menuVariable ;
+    this.menu_icon_variable !=this.menu_icon_variable;
+  }
+
+}
+    
